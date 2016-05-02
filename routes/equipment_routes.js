857bb708338d4e8402cfd3910d89e552d7b37703
Adam Wright <leurcutis@gmail.com>
2016-05-02 14:50:51 -0700

    new equipment branch

---
+++ routes/equipment_routes.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var equipmentController = require('../controllers/equipment_controller.js');
+console.log(equipmentController);
+
+//posts urls inputs to our model
+router.route('/')
+  .get(equipmentController.index)
+  .post(equipmentController.create);
+
+// router.route('/:id')
+//   .get(linksController.show)
+//   .patch(linksController.update);
+
+
+module.exports = router;
