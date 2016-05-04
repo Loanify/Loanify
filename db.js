482ae4619f8f21c7d67122fb5e5b177d07fc34c5
Adam Wright <leurcutis@gmail.com>
2016-05-03 17:45:13 -0700

    fixed heroku

--- db.js
+++ db.js
@@ -1,13 +1,15 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/Loanify', function(err){
+var mongo_uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/Loanify';
+
+mongoose.connect(mongo_uri, function(err){
   if (err){
     console.log("Cannot connect to database", err);
 
   }else{
-    console.log('Database connected.')
+    console.log('Database connected.');
   }
   var db = mongoose.connection;
-})
+});
 
-module.exports = mongoose
+module.exports = mongoose;
