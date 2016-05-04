ee72b0c9f4cc6a0688812fdf7623dc8f555c713f
David Fisher <davie.fisher@gmail.com>
2016-05-04 16:25:10 -0700

    person routes

--- routes/person_routes.js
+++ routes/person_routes.js
@@ -4,19 +4,35 @@ var personController = require('../controllers/person_controller.js');
 console.log(personController);
 
 //posts urls inputs to our model
-router.route('/person/index')
-  .get(personController.index);
-
-router.route('/person/new')
-  .get(personController.new)
+router.route('/')
+  .get(personController.index)
   .post(personController.create);
+
+router.route('/new')
+  .get(personController.new);
 
 // router.route('/:id')
 //   .get(personController.show)
 //   .patch(linksController.update);
-router.get('/show', function(req, res) {
-  res.render('show');
-});
-  // personController.show);
+router.get('/:id')
+  .get(personController.show)
+  .post(personController.update);
 
 module.exports = router;
+
+
+// //posts urls inputs to our model
+// router.route('/index')
+//   .get(equipmentController.index);
+
+// router.route('/new')
+//   .get(equipmentController.new);
+
+// router.route('/')
+//   .post(equipmentController.create);
+
+// router.route('/:id')
+//   .delete(equipmentController.destroy)
+//   .get(equipmentController.show)
+//   .post(equipmentController.update);
+
