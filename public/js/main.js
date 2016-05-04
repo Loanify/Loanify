11732300f8b1bb0aad04a4d8f9cc71ee91e2f585
Robert Springer <springerdrums@gmail.com>
2016-05-03 17:14:05 -0700

    ber

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,20 @@
 console.log('hello')
+$.ajax({
+  url: '/equipment/index'
+  })
+.done(function(data) {
+  data.forEach(function(data) {
+    $('#printer').append('<li>' + equipment.itemTitle + '</li>');
+  });
+});
+$('#submit1').on('click', function() {
+  var val = $('#itemTitle').val();
+  $.ajax({
+    url: '/equipment/new',
+    method: 'POST',
+    data: {itemTitle: val},
+    success: function(data) {
+      $('#printer').append('<li>' + equipment.data '</li>');
+    }
+  });
+});
