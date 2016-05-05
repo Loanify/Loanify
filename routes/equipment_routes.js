855ba1b7996c4ef7a64f7bb19a005dd1f541a3fd
david szyszkowski <try2murkme@gmail.com>
2016-05-05 11:43:34 -0700

    deleted index route

--- routes/equipment_routes.js
+++ routes/equipment_routes.js
@@ -5,23 +5,17 @@ var equipmentController = require('../controllers/equipment_controller.js');
 console.log(equipmentController);
 
 //posts urls inputs to our model
-router.route('/index')
-  .get(equipmentController.index);
-
 router.route('/new')
   .get(equipmentController.new);
 
 router.route('/')
+  .get(equipmentController.index)
   .post(equipmentController.create);
 
 router.route('/:id')
   .delete(equipmentController.destroy)
   .get(equipmentController.show)
   .post(equipmentController.update);
-
-// router.route('/:id')
-//   .get(linksController.show)
-//   .patch(linksController.update);
 
 
 module.exports = router;
