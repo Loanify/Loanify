d06e2fea2ea579226b3aef4bccd415034b4c9f7d
Adam Wright <leurcutis@gmail.com>
2016-05-05 16:30:43 -0700

    working out cronjob email

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express= require('express');
 var logger = require('morgan');
 var app = express();
+var Equipment = require('./models/equipment_model');
 var nodemailer = require("nodemailer");
 var smtpTransport = require('nodemailer-smtp-transport');
 
@@ -23,6 +24,7 @@ var flash    = require('connect-flash');
 var router = require('./routes/user_routes');
 var equipment_routes = require('./routes/equipment_routes');
 var person_routes = require('./routes/person_routes');
+var CronJob = require('cron').CronJob;
 
 //Configure view engine to render EJS templates
 app.set('views', __dirname + '/views');
@@ -66,6 +68,65 @@ app.get('/send',function(req,res){
   });
 });
 
+var job = new CronJob({
+  cronTime: '00 40 14 * * 1-5',
+  onTick: function() {
+    var nodemailer = require("nodemailer"),
+      transport = nodemailer.createTransport('direct', {
+        debug: true, //this!!!
+  });
+
+      transport.sendMail({
+        from: "Fred Foo <foo@blurdybloop.com>", // sender address
+        // put new array here
+        to: notAvialable, // list of receivers
+        subject: "Hello", // Subject line
+        text: "Hey DICKHEAD", // plaintext body
+        html: "<b>GIVE US BACK OUR EQUIPMENT OR YOUR FUCKED</b>" // html body
+    }, console.error);
+  },
+  start: true,
+  timeZone: 'America/Los_Angeles'
+});
+
+//set interval
+var notAvailable = [];
+
+var emailRemind = function() {
+  // var equipment;
+  Equipment.find({}, function(err, equipment){
+  console.log(equipment);
+    if (err) {
+      throw err;
+    } else {
+      for (var i = 0; i < equipment.length; i++) {
+        if (!equipment.available) {
+            var equip = equipment;
+            console.log(equip.available);
+            // notAvailable.push(equipment.loanedTo.email[i]);
+          job.start();
+        }
+      }
+    }
+  });
+};
+emailRemind();
+  // loop through all equipment entries
+  // if checked out, push email to new array
+  // if (!equipment.loanedTo.available) {
+  //     job.start();
+  //   }
+
+
+
+
+// emailRemind();
+
 app.listen(port, function(){
   console.log("Listening on port " + port);
 });
+    /*
+     * Runs every weekday (Monday through Friday)
+     * at 11:30:00 AM. It does not run on Saturday
+     * or Sunday.
+     */
